b1666e1b246fcdf7f74d4b42b066cd4aaa70ed28
James Vickery <dev@jamesvickery.net>
2020-01-29 16:34:39 +0000

    make the thingy part of the autorefresh

--- scripty.js
+++ scripty.js
@@ -9,11 +9,9 @@ const intervalTime = 1000
     i = (i + 1) % (intervals + 1)
 
     progressBar.style.width = `${i * (100 / intervals)}%`
+
+    if (Math.random() > 0.94) {
+      window.location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+    }
   }, intervalTime)
 })()
-
-setTimeout(() => {
-  if (Math.random() > 0.94) {
-    window.location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
-  }
-}, 4000)
